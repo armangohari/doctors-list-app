a36f0ca2005ea1e0eb41ef2152f919bf12a99589
armangohari <arman.gohari.2001@gmail.com>
2024-07-03 15:15:23 +0330

    feat: add easy to access icons file

---
+++ src/lib/icons.ts
@@ -0,0 +1,15 @@
+import addCommentEmpty from "../../public/assets/icons/add-comment-empty.svg";
+import addCommentFull from "../../public/assets/icons/add-comment-full.svg";
+import closeCommentEmpty from "../../public/assets/icons/close-comment-empty.svg";
+import closeCommentFull from "../../public/assets/icons/close-comment-full.svg";
+import defaultProfileImage from "../../public/assets/icons/default-profile-image.svg";
+import heart from "../../public/assets/icons/heart.svg";
+
+export {
+  addCommentEmpty,
+  addCommentFull,
+  closeCommentEmpty,
+  closeCommentFull,
+  defaultProfileImage,
+  heart,
+};
