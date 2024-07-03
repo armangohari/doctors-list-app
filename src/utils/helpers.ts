82bbd66d9db7d0a0118c9e7ca43555c9743b8ac4
armangohari <arman.gohari.2001@gmail.com>
2024-07-03 15:17:49 +0330

    feat: add capitalizeWords helper function, enhance the helper functions documentation

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,22 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// The `cn` function takes any number of arguments of type `ClassValue`,
-// merges them using `clsx`, and then merges the resulting class names using `twMerge`.
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Combines and merges class names using `clsx` and `twMerge`.
+ *
+ * @param {...ClassValue[]} inputs - An arbitrary number of class values to be merged.
+ * @returns {string} - A single string of combined and merged class names.
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
+}
+
+/**
+ * Capitalizes the first letter of each word in a string.
+ *
+ * @param {string} str - The input string to be transformed.
+ * @returns {string} - The transformed string with the first letter of each word capitalized.
+ */
+export function capitalizeWords(str: string): string {
+  return str.replace(/\b\w/g, (char) => char.toUpperCase());
 }
