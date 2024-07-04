0ecec31f9428e88e7e7757a885cf09a9309d3f56
armangohari <arman.gohari.2001@gmail.com>
2024-07-04 10:48:42 +0330

    feat: add back icon for doctor comment section

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,5 +1,6 @@
 import addCommentEmpty from "../../public/assets/icons/add-comment-empty.svg";
 import addCommentFull from "../../public/assets/icons/add-comment-full.svg";
+import back from "../../public/assets/icons/back.svg";
 import closeCommentEmpty from "../../public/assets/icons/close-comment-empty.svg";
 import closeCommentFull from "../../public/assets/icons/close-comment-full.svg";
 import comment from "../../public/assets/icons/comment.svg";
@@ -9,6 +10,7 @@ import heart from "../../public/assets/icons/heart.svg";
 export {
   addCommentEmpty,
   addCommentFull,
+  back,
   closeCommentEmpty,
   closeCommentFull,
   comment,
