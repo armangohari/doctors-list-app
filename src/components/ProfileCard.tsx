29365ef0bd0bed9387613f99699892a363424602
armangohari <arman.gohari.2001@gmail.com>
2024-07-04 10:50:31 +0330

    feat: add doctor comment viewer functionality

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { axiosBase } from "@/axios/config";
 import {
   addCommentEmpty,
   addCommentFull,
   closeCommentEmpty,
   closeCommentFull,
+  comment,
   heart,
 } from "@/lib/icons";
 import { cn } from "@/utils/helpers";
 import Image, { StaticImageData } from "next/image";
-import { FormEvent, useState } from "react";
+import { useState } from "react";
+import DoctorCommentsModal from "./DoctorCommentsModal";
 
 export type doctorType = {
   id: number;
@@ -18,100 +21,135 @@ export type doctorType = {
   specialization: string;
   profileImage: StaticImageData;
   numOfLikes: number;
+  numOfComments: number;
 };
 
 export default function ProfileCard({
+  id,
   name,
   email,
   specialization,
   profileImage,
   numOfLikes,
+  numOfComments,
 }: doctorType) {
+  const [isCommentModalVisible, setIsCommentModalVisible] =
+    useState<boolean>(false);
   const [isCommentAreaActive, setIsCommentAreaActive] =
     useState<boolean>(false);
   const [commentText, setCommentText] = useState<string>("");
 
-  function toggleCommentAreaVisibility() {
-    setIsCommentAreaActive((prev) => !prev);
-  }
-
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log(commentText);
-    // todo: handle the rest of submission
+  async function handleSubmit() {
+    await axiosBase
+      .post("/comments", {
+        doctorId: id,
+        content: commentText,
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   return (
-    <article className="w-96 rounded-xl bg-cream-white p-6 max-sm:scale-90">
-      <div className="flex items-start justify-start gap-6">
-        <div className="flex min-w-20 flex-col items-center justify-between gap-3">
-          {/* Doctor Profile Image */}
-          <Image
-            src={profileImage}
-            alt={name}
-            width={80}
-            height={80}
-            className="rounded-full border-2 border-white"
-          />
-          <div className="mt-2 flex gap-0.5 text-sm font-semibold">
-            <Image src={heart} alt="heart" className="h-5 w-5" />
-            <span>{numOfLikes}</span>
+    <article className="w-96 rounded-xl bg-cream-white p-4 max-sm:scale-90">
+      {isCommentModalVisible ? (
+        <DoctorCommentsModal
+          doctorId={id}
+          isVisible={isCommentModalVisible}
+          setIsVisible={setIsCommentModalVisible}
+        />
+      ) : (
+        <div className="flex items-start justify-start gap-6">
+          <div className="flex min-w-20 flex-col items-center justify-between gap-3">
+            {/* Doctor Profile Image */}
+            <Image
+              src={profileImage}
+              alt={name}
+              width={80}
+              height={80}
+              className="rounded-full border border-gray-200 bg-white"
+            />
+
+            <div className="mt-[5px] flex gap-2">
+              {/* Doctor Likes */}
+              <div className="flex gap-0.5 p-1 text-sm font-semibold">
+                <Image src={heart} alt="heart" className="h-5 w-5" />
+                <span>{numOfLikes}</span>
+              </div>
+
+              {/* Doctor Comments */}
+              <button
+                className={cn(
+                  "flex gap-0.5 rounded-md bg-green-300/20 p-1 text-sm font-semibold transition-all",
+                  "hover:bg-green-300/50",
+                  "active:scale-95",
+                )}
+                onClick={() => {
+                  setIsCommentModalVisible(true);
+                  setIsCommentAreaActive(false);
+                }}
+              >
+                <Image src={comment} alt="heart" className="h-5 w-5" />
+                <span>{numOfComments}</span>
+              </button>
+            </div>
+          </div>
+          {/* Doctor Information */}
+          <div className="flex flex-col items-start justify-between gap-2">
+            <h1 className="max-w-60 truncate font-extrabold" title={name}>
+              {name}
+            </h1>
+            <h2
+              className="max-w-60 truncate font-semibold"
+              title={specialization}
+            >
+              {specialization}
+            </h2>
+            <h3 className="max-w-60 truncate font-light" title={email}>
+              {email}
+            </h3>
+
+            <button
+              className={cn(
+                "flex items-center justify-center gap-2 rounded-lg bg-green-100/50 px-1 py-0.5 transition-all",
+                "hover:bg-green-300/50",
+                "active:scale-95 active:shadow-inner",
+              )}
+              onClick={() => setIsCommentAreaActive((prev) => !prev)}
+            >
+              {isCommentAreaActive ? (
+                <>
+                  {/* Set the close comment icon based on the emptiness state of comment text area */}
+                  <Image
+                    src={commentText ? closeCommentFull : closeCommentEmpty}
+                    alt="Close comment"
+                    className="h-5 w-5"
+                  />
+                  <span>Close Comment</span>
+                </>
+              ) : (
+                <>
+                  {/* Set the add comment icon based on the emptiness state of comment text area */}
+                  <Image
+                    src={commentText ? addCommentFull : addCommentEmpty}
+                    alt="Add comment"
+                    className="h-5 w-5"
+                  />
+                  <span>Add Comment</span>
+                </>
+              )}
+            </button>
           </div>
         </div>
-        {/* Doctor Information */}
-        <div className="flex flex-col items-start justify-between gap-2">
-          <h1 className="max-w-60 truncate font-extrabold" title={name}>
-            {name}
-          </h1>
-          <h2
-            className="max-w-60 truncate font-semibold"
-            title={specialization}
-          >
-            {specialization}
-          </h2>
-          <h3 className="max-w-60 truncate font-light" title={email}>
-            {email}
-          </h3>
-
-          <button
-            className={cn(
-              "flex items-center justify-center gap-2 rounded-lg bg-green-100/50 px-1 py-0.5 transition-all",
-              "active:scale-95 active:shadow-inner",
-            )}
-            onClick={toggleCommentAreaVisibility}
-          >
-            {isCommentAreaActive ? (
-              <>
-                {/* Set the close comment icon based on the emptiness state of comment text area */}
-                <Image
-                  src={commentText ? closeCommentFull : closeCommentEmpty}
-                  alt="Close comment"
-                  className="h-5 w-5"
-                />
-                <span>Close Comment</span>
-              </>
-            ) : (
-              <>
-                {/* Set the add comment icon based on the emptiness state of comment text area */}
-                <Image
-                  src={commentText ? addCommentFull : addCommentEmpty}
-                  alt="Add comment"
-                  className="h-5 w-5"
-                />
-                <span>Add Comment</span>
-              </>
-            )}
-          </button>
-        </div>
-      </div>
+      )}
 
       {/* Add Comment Form */}
       <form
         className={cn(
-          "mt-2 hidden flex-col items-start justify-start gap-2",
+          "mt-3 hidden flex-col items-start justify-start gap-2",
           isCommentAreaActive && "block",
         )}
-        onSubmit={(e) => handleSubmit(e)}
+        onSubmit={handleSubmit}
       >
         {/* Comment Textarea */}
         <textarea
@@ -122,6 +160,7 @@ export default function ProfileCard({
           placeholder="Describe your satisfaction . . ."
           value={commentText}
           onChange={(e) => setCommentText(e.target.value)}
+          required
         />
 
         {/* Comment Submit Button */}
