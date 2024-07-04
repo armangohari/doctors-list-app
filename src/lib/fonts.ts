43e340c41f09ee3fe055841f54bc2225174e0289
armangohari <arman.gohari.2001@gmail.com>
2024-07-04 10:45:48 +0330

    fix: add Poppins as local font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,6 +1,51 @@
-import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
 
-export const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+export const poppins = localFont({
+  src: [
+    {
+      path: "../../public/assets/fonts/Poppins-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Poppins-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Poppins-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Poppins-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Poppins-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Poppins-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Poppins-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Poppins-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Poppins-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
 });
